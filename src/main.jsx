42208defa6f2151030726497a6429016d2a8584c
nadimmo <nadimmostafa334@gmail.com>
2024-12-06 19:20:18 +0600

    added data form database in event page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import Router from './Route/Route'
-import AuthProvider from './AuthProvider/AuthProvider'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Route/Route";
+import AuthProvider from "./AuthProvider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-    <RouterProvider router={Router} />
-    </AuthProvider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={Router} />
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
